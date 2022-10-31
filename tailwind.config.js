97fcbc9f8667d7e01321c5ef8ac1a1e4fd7bf960
Divine Okafor-udah <deeokafor@DivineOkafor-udah.localdomain>
2022-10-31 19:52:46 +0100

    finished scaffolding for sidebar

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //content: ["./{index.html,app.js}"],
+  content: ["./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: "tw-"
+}
